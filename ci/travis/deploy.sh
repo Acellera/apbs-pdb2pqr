efeef86f22398d465ca896b4ad6d0346b1d934f0
Stefan Doerr <stefdoerr@gmail.com>
2020-02-10 12:32:04 +0200

    changes for travis build

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -7,6 +7,6 @@ export TAG_DESCRIBE=$(git describe)
 export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
 export BUILD_NUMBER=0
 
-conda build --python 3.5 package/htmd-pdb2pqr/ -c acellera
+conda build --python $CONDA_PY package/htmd-pdb2pqr/ -c acellera
 
 bash -x ./ci/travis/conda_upload.sh;

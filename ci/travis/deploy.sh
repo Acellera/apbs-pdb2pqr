45a795eb2c70e00091418971cfa9b10dc0430154
Stefan Doerr <stefdoerr@gmail.com>
2020-05-19 10:36:27 +0300

    fix travis build

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Build for conda from PyPI
-source deactivate test
-
 export TAG_DESCRIBE=$(git describe)
 export BUILD_VERSION=$(echo $TAG_DESCRIBE | sed 's/-/ /g'  | awk '{print $1}')
 export BUILD_NUMBER=0
